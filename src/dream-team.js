75e80db0ed16afb3c290ecd0d4ee21bc68e41434
Kate <chelsea-92@mail.ru>
2020-03-04 00:24:01 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let dreamTeam = '';
+    
+    members.forEach(member => {
+      if (typeof member === "string") {
+        member = member.trim();
+        dreamTeam += member[0];
+      }
+    });
+    return dreamTeam.toUpperCase().split('').sort().join('');
+  } else {
+    return false;
+  }
+}
