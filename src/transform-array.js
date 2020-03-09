cf26e4dd8890c799c83a735ba920e3994c66cf4f
Kate <chelsea-92@mail.ru>
2020-03-10 02:02:09 +0300

    feat: implement transform(arr) function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+        case '--discard-next':
+          i++;
+          break;
+        case '--discard-prev':
+          if (res.length) {
+          	res.pop();
+          }
+          break;
+        case '--double-next':
+          if (i < arr.length - 1) {
+          	res.push(arr[i + 1]);
+          }
+          break;
+        case '--double-prev':
+          if (i) {
+          	res.push(arr[i - 1]);
+          }
+          break;
+        default:
+        	res.push(arr[i]);
+      }
+    }
+    
+    return res;
+  } else {
+    throw Error;
+  }
 };
