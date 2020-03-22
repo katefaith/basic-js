fafb856838bb7f3599976e5f751141600590a39e
Kate <chelsea-92@mail.ru>
2020-03-22 21:58:20 +0300

    feat: solve Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,66 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isDirect) {
+        if (isDirect === true || typeof isDirect === 'undefined') {
+            this.isDirect = true
+        } else {
+            this.isDirect = false
+        }
+        this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    loopKey(message, key) {
+        let loopedKey = '',
+            keyIndex = 0;
+        for (let i = 0; i < message.length; i++) {
+            if (this.alphabet.indexOf(message[i]) < 0) {
+                loopedKey += message[i]
+            } else {
+                loopedKey += key[keyIndex]
+                keyIndex++
+                if (keyIndex > key.length - 1) {
+                    keyIndex = 0
+                }
+            }
+        }
+        return loopedKey
+    }
+
+    encrypt(message, key) {
+        let encryptedMessage = ''
+        if (typeof message === 'undefined' || typeof key === 'undefined') throw Error
+
+        message = message.toUpperCase()
+        key = this.loopKey(message, key.toUpperCase())
+
+        for (let i = 0; i < message.length; i++) {
+            if (this.alphabet.indexOf(message[i]) >= 0) {
+                const index = (this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(key[i])) % this.alphabet.length
+                encryptedMessage += this.alphabet[index]
+            } else {
+                encryptedMessage += message[i]
+            }
+        }
+
+        return (this.isDirect) ? encryptedMessage : encryptedMessage.split('').reverse().join('')
+    }
+
+    decrypt(encryptedMessage, key) {
+        let message = ''
+        if (typeof encryptedMessage === 'undefined' || typeof key === 'undefined') throw Error
+
+        encryptedMessage = encryptedMessage.toUpperCase()
+        key = this.loopKey(encryptedMessage, key.toUpperCase())
+
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            if (this.alphabet.indexOf(encryptedMessage[i]) >= 0) {
+                const index = (this.alphabet.indexOf(encryptedMessage[i]) + this.alphabet.length - this.alphabet.indexOf(key[i])) % this.alphabet.length
+                message += this.alphabet[index]
+            } else {
+                message += encryptedMessage[i]
+            }
+        }
+
+        return (this.isDirect) ? message : message.split('').reverse().join('')
     }
 }
 
